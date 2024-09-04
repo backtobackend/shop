9bd1f725099cae73301e5610e5024fa86534bbdc
ivan <block4ain226@gmail.com>
2024-09-04 15:30:52 +0300

    refactor: configuration.config.ts file using env

--- src/config/configuration.config.ts
+++ src/config/configuration.config.ts
@@ -1,13 +1,14 @@
-import {dataSourceConfig} from '../database/datasource.config';
+import * as process from 'node:process';
+import 'dotenv/config'
 
 export default () => ({
     database: {
-        type: 'postgres',
-        host: 'localhost',
-        port: 35432,
-        username: 'postgres',
-        password: '796163',
-        database: 'shop',
+        type: process.env.POSTGRESQL_TYPE,
+        host: process.env.POSTGRESQL_HOST,
+        port: process.env.POSTGRESQL_PORT,
+        username: process.env.POSTGRESQL_USERNAME,
+        password: process.env.POSTGRESQL_PASSWORD,
+        database: process.env.POSTGRESQL_DATABASE,
         entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
         migrations: [`${__dirname}/../../db/migrations/*{.ts,.js}`],
         // autoLoadEntities: true,
