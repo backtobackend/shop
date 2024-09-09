9e9d967597af84b4d840a858932959007b98add2
ivan <block4ain226@gmail.com>
2024-09-09 12:09:22 +0300

    feat: add createProductDto, responseProductDto

--- src/product/interface/product.interface.ts
+++ src/product/interface/product.interface.ts
@@ -1,5 +1,6 @@
 import {IBaseCrud} from '../../interfaces/base-crud.interface';
 import {CreateProductDto} from '../dto/create-product.dto';
+import {ResponseProductDto} from '../dto/response-product.dto';
 
 export interface IProductCrud extends IBaseCrud<CreateProductDto, ResponseProductDto> {
 }
